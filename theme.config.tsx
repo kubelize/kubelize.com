e7b0c57e811af8a6607c6b01c135560590bfaf30
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-03-09 15:59:43 +0100

    chore: adding kubelize.png as logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,24 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>kubelize</span>,
+  // logo: <span>kubelize</span>,
+
+  logo: (
+    <>
+      {/* PNG logo with alt text and styling */}
+      <img
+        src="/.github/kubelize.png"
+        alt="Logo"
+        style={{ width: '24px', height: '24px', marginRight: '4px' }}
+      />
+
+      {/* Additional text with styling */}
+      <span style={{ fontWeight: 800 }}>
+        kubelize
+      </span>
+    </>
+  )
+
   project: {
     link: 'https://github.com/kubelize',
   },
