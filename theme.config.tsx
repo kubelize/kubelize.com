c81c75ca06bcba40c26c1158b61172507cf9a19b
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-03-09 12:13:57 +0100

    chore: initial adjustements, to learn how nextra works

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>kubelize</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/kubelize/kubelize.com',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/kubelize/kubelize.com',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'kubelize.com',
   },
 }
 
